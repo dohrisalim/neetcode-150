12a6a009b2ca575556e10c88137d79bf0f207557
Salim Dohri <dohri.salim@gmail.com>
2024-01-09 13:03:02 +0100

    Add sliding window solution for checking string inclusion

---
+++ sliding window/567.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_count = Counter(s1)
+        s2_count = Counter(s2[:len(s1)])
+
+        if s1_count == s2_count:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            s2_count[s2[i]] += 1
+            s2_count[s2[i - len(s1)]] -= 1
+            if s2_count[s2[i - len(s1)]] == 0:
+                del s2_count[s2[i - len(s1)]]
+            if s1_count == s2_count:
+                return True
+
+        return False
